6a0855fa22e0ce4ab21ee933a749e47d847eb4c9
StealthElf98 <sergej.dragojlovic@gmail.com>
2022-06-12 23:06:54 +0200

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Sergej on 12.6.22..
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
